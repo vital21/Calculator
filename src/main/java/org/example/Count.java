5f4a8d1b333308f622483b4565749e647288b65f
Vitaliy <vitaliysaydak@gmail.com>
2023-04-07 12:17:17 +0300

    метод рекурсивного спуска

--- src/main/java/org/example/Count.java
+++ src/main/java/org/example/Count.java
@@ -9,6 +9,7 @@ public class Count {
         LEFT_BRACKET, RIGHT_BRACKET,
         OP_PLUS, OP_MINUS, OP_DOLLARS, OP_RUBLE,
         NUMBER,
+        OP_MUL,OP_DIV,
         EOF;
     }
     private double result;
@@ -20,12 +21,12 @@ public class Count {
         List<Lexeme> lexemes = new ArrayList<>();
         lexemes=stringRedact();
         LexemeBuffer leksbuf=new LexemeBuffer(lexemes);
-        //  double result=factor(leksbuf);
+        double result=expr(leksbuf);
         return result;
     }
     public static List<Lexeme> stringRedact(){
         List<Lexeme> lexemes = new ArrayList<>();
-        StringTokenizer str= new StringTokenizer(input,"()+pр $",true);
+        StringTokenizer str= new StringTokenizer(input,"()+pр $*/",true);
         String test="";
         while (str.hasMoreTokens()){
              test=String.format(str.nextToken());
@@ -48,12 +49,15 @@ public class Count {
                     continue;
                 case "-" :
                     lexemes.add(new Lexeme(LexemeType.OP_MINUS,test));
+                case "*":
+
                 case "p":
                     continue;
                 case "$":
                     continue;
                 case " ":
                     continue;
+
                 default:
 
                     if(Double.valueOf(test)>=0){
@@ -111,10 +115,108 @@ public class Count {
             return pos;
         }
     }
+    public static double expr(LexemeBuffer lexemes) {
+        Lexeme lexeme = lexemes.next();
+        if (lexeme.type == LexemeType.EOF) {
+            return 0;
+        } else {
+            lexemes.back();
+            return plusminus(lexemes);
+        }
+    }
+    public static double plusminus(LexemeBuffer lexemes) {
+        double value = multdiv(lexemes);
+        while (true) {
+            Lexeme lexeme = lexemes.next();
+            switch (lexeme.type) {
+                case OP_PLUS:
+                    value += multdiv(lexemes);
+                    break;
+                case OP_MINUS:
+                    value -= multdiv(lexemes);
+                    break;
+                case EOF:
+                case RIGHT_BRACKET:
+                    lexemes.back();
+                    return value;
+                default:
+                    throw new RuntimeException("Unexpected token: " + lexeme.value
+                            + " at position: " + lexemes.getPos());
+            }
+        }
+    }
+    public static double multdiv(LexemeBuffer lexemes) {
+        double value = factor(lexemes);
 
+        while (true) {
+            Lexeme lexeme = lexemes.next();
+            switch (lexeme.type) {
+                case OP_MUL:
+                    value *= toVal(lexemes);
+                    break;
+                case OP_DIV:
+                    value /= toVal(lexemes);
+                    break;
+                case EOF:
+                case RIGHT_BRACKET:
+                case OP_PLUS:
+                case OP_MINUS:
+                    lexemes.back();
+                    return value;
+                default:
+                    throw new RuntimeException("Unexpected token: " + lexeme.value
+                            + " at position: " + lexemes.getPos());
+            }
+        }
+    }
+    public static double toVal(LexemeBuffer lexemes) {
+        double value = factor(lexemes);
+        double dollars=0.5;
+        double ruble=0.2;
+        while (true) {
+            Lexeme lexeme = lexemes.next();
+            switch (lexeme.type) {
+                case OP_DOLLARS:
+                    dollars*=factor(lexemes);
+                case OP_RUBLE:
+                    ruble*=factor(lexemes);
+                case OP_MUL:
+                case OP_DIV:
+                case EOF:
+                case RIGHT_BRACKET:
+                case OP_PLUS:
+                case OP_MINUS:
+                    lexemes.back();
+                    return value;
+                default:
+                    throw new RuntimeException("Unexpected token: " + lexeme.value
+                            + " at position: " + lexemes.getPos());
+            }
+        }
+    }
 
-
-
-
+    public static double factor(LexemeBuffer lexemes) {
+        Lexeme lexeme = lexemes.next();
+        double value=0;
+        switch (lexeme.type) {
+            case OP_RUBLE:
+                value =toVal(lexemes);
+            case OP_DOLLARS:
+                value =toVal(lexemes);
+            case NUMBER:
+                return Double.parseDouble(lexeme.value);
+            case LEFT_BRACKET:
+                 value = plusminus(lexemes);
+                lexeme = lexemes.next();
+                if (lexeme.type != LexemeType.RIGHT_BRACKET) {
+                    throw new RuntimeException("Unexpected token: " + lexeme.value
+                            + " at position: " + lexemes.getPos());
+                }
+                return value;
+            default:
+                throw new RuntimeException("Unexpected token: " + lexeme.value
+                        + " at position: " + lexemes.getPos());
+        }
+    }
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String input="toDollars(737p + toRubles($85.4))";
+        String input="25+25*(14+17)";
         Count count = new Count(input);
         count.stringRedact();
         count.res();
